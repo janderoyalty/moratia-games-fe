2abc58dec899d85b5e9a214af7866ee8ab378ef8
Jande Royalty <janderoyalty@gmail.com>
2022-10-26 02:06:25 -0500

    adds scrolling to map of moratia section

--- src/components/World.js
+++ src/components/World.js
@@ -5,17 +5,15 @@ import WorldText from "./WorldText";
 const World = () => {
   return (
     <div className="content" id="world">
-      <div id="map">
-        <>
-          <img
-            id="map-image"
-            src="https://res.cloudinary.com/dvbdefnwx/image/upload/v1666678438/Moratia%20Images/IMG_4098_w65sgn.png"
-            alt="map of the world of Moratia"
-          ></img>
-          <div id='text-on-image'>
-            <WorldText></WorldText>
-          </div>
-        </>
+      <div id="world--map">
+        <img
+          id="world--map-image"
+          src="https://res.cloudinary.com/dvbdefnwx/image/upload/v1666678438/Moratia%20Images/IMG_4098_w65sgn.png"
+          alt="map of the world of Moratia"
+        ></img>
+        <div id="world--map--text">
+          <WorldText></WorldText>
+        </div>
       </div>
     </div>
   );
