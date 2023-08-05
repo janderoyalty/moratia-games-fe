72fe8973ec35b31410ef70d22ab25b7c6a0158d7
Jande Royalty <janderoyalty@gmail.com>
2023-08-05 11:23:39 -0400

    Fixes video not being responsive

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,8 +4,8 @@ const EmbeddedVideo = ({embedId}) => {
   return (
 <div className="video-responsive">
     <iframe
-      width="853"
-      height="480"
+      // width="853"
+      // height="480"
       src={`https://www.youtube.com/embed/${embedId}`}
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
