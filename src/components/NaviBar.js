6b7390f9eb40f51dbd504830003bc4b83a95df36
Jande Royalty <janderoyalty@gmail.com>
2024-07-14 23:08:35 -0500

    Add shopify link

--- src/components/NaviBar.js
+++ src/components/NaviBar.js
@@ -1,9 +1,27 @@
-import React from "react";
+import { React, useState } from "react";
 import "./NaviBar.css";
+import { Modal, Button } from "react-bootstrap";
 
 import { Nav, Navbar, Container } from "react-bootstrap";
 
 const NaviBar = () => {
+	const [showModal, setShowModal] = useState(false);
+	const [linkToOpen, setLinkToOpen] = useState("");
+
+	const handleLinkClick = (e, link) => {
+		e.preventDefault();
+		setLinkToOpen(link);
+		setShowModal(true);
+	};
+
+	const handleConfirm = () => {
+		window.open(linkToOpen, "_blank", "noopener,noreferrer");
+		setShowModal(false);
+	};
+
+	const handleClose = () => {
+		setShowModal(false);
+	};
 	return (
 		<>
 			<Navbar
@@ -26,6 +44,16 @@ const NaviBar = () => {
 					<Navbar.Toggle aria-controls="navbarScroll" />
 					<Navbar.Collapse id="navbarScroll">
 						<Nav className="ms-auto my-2 my-lg-0" navbarScroll>
+							<Nav.Link
+								id="shopify-button"
+								target="_blank"
+								rel="noopener noreferrer "
+								onClick={(e) =>
+									handleLinkClick(e, "https://moratiagames.myshopify.com/")
+								}
+							>
+								Shop
+							</Nav.Link>
 							<Nav.Link href="#updates">Updates</Nav.Link>
 							<Nav.Link href="#gallery">Gallery</Nav.Link>
 							<Nav.Link href="#world">World</Nav.Link>
@@ -39,6 +67,23 @@ const NaviBar = () => {
 					</Navbar.Collapse>
 				</Container>
 			</Navbar>
+			<Modal show={showModal} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>Leaving Our Website</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					You are about to leave our website to go to our Shopify store. Do you
+					want to continue?
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleClose}>
+						Cancel
+					</Button>
+					<Button variant="success" onClick={handleConfirm}>
+						Shopify
+					</Button>
+				</Modal.Footer>
+			</Modal>
 		</>
 	);
 };
--- src/components/Release.js
+++ src/components/Release.js
@@ -1,26 +1,64 @@
-import React from "react";
+import { React, useState } from "react";
 import "./Release.css";
-import { Row, Col, Image, Button } from "react-bootstrap";
+import { Row, Image, Modal, Button } from "react-bootstrap";
 
 const Release = () => {
+	const [showModal, setShowModal] = useState(false);
+	const [linkToOpen, setLinkToOpen] = useState("");
+
+	const handleLinkClick = (e, link) => {
+		e.preventDefault();
+		setLinkToOpen(link);
+		setShowModal(true);
+	};
+
+	const handleConfirm = () => {
+		window.open(linkToOpen, "_blank", "noopener,noreferrer");
+		setShowModal(false);
+	};
+
+	const handleClose = () => {
+		setShowModal(false);
+	};
+
 	return (
-		<Row className="content" id="release">
-			<Row className="headers-text" id="preorder-top">
-				Releasing July 15
+		<>
+			<Row className="content" id="release">
+				<Row className="headers-text" id="preorder-top">
+					Available Now
+				</Row>
+				<Row id="release-bottom">
+					<Image
+						src={
+							"https://firebasestorage.googleapis.com/v0/b/moratia-games.appspot.com/o/Moratia_Card_Quest_Games_Base._Game_Cover.png?alt=media&token=78c1896a-355c-4bc9-b441-c357b60109e8"
+						}
+						alt={
+							"Moratia Games image for Kickstarter with outline of person with bow and arrow on hill and Moratia Card Quest Game logo over colorful backgroud"
+						}
+						rounded
+						id="preorder-image"
+						onClick={handleLinkClick}
+					></Image>
+				</Row>
 			</Row>
-			<Row id="release-bottom">
-				<Image
-					src={
-						"https://firebasestorage.googleapis.com/v0/b/moratia-games.appspot.com/o/Moratia_Card_Quest_Games_Base._Game_Cover.png?alt=media&token=78c1896a-355c-4bc9-b441-c357b60109e8"
-					}
-					alt={
-						"Moratia Games image for Kickstarter with outline of person with bow and arrow on hill and Moratia Card Quest Game logo over colorful backgroud"
-					}
-					rounded
-					id="preorder-image"
-				></Image>
-			</Row>
-		</Row>
+			<Modal show={showModal} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>Leaving Our Website</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					You are about to leave our website to go to our Shopify store. Do you
+					want to continue?
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleClose}>
+						Cancel
+					</Button>
+					<Button variant="success" onClick={handleConfirm}>
+						Shopify
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</>
 	);
 };
 
