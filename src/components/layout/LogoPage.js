8a207114fe4de1a85e006f8c12f6eb509e48e59f
Jande Royalty <janderoyalty@gmail.com>
2025-04-18 03:39:37 -0500

    feat: update logo page background image and styling

--- src/components/layout/LogoPage.js
+++ src/components/layout/LogoPage.js
@@ -4,13 +4,7 @@ import "./LogoPage.css";
 const LogoPage = () => {
 	return (
 		<div className="content" id="home">
-			<div id="logo-container">
-				<img
-					id="landing-logo"
-					src="https://firebasestorage.googleapis.com/v0/b/moratia-games.appspot.com/o/Moratia_Games_logo.png?alt=media&token=246499e7-740e-42c0-9bf0-308085e6fbed"
-					alt="Moratia Games logo black"
-				/>
-			</div>
+			<div id="logo-container"></div>
 		</div>
 	);
 };
