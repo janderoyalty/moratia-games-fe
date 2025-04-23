24b31917755fbf092c14ad9b7064898cbe3dbbc4
Jande Royalty <janderoyalty@gmail.com>
2025-04-23 12:49:49 -0500

    feature: add contact us

---
+++ src/components/marketing/ContactUs.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ContactUs.css";
+import ContactForm from "./ContactForm";
+
+const ContactUs = () => {
+	return (
+		<div className="content" id="contact-us">
+			<div className="template-container">
+				<ContactForm />
+			</div>
+		</div>
+	);
+};
+
+export default ContactUs;
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,8 +1,15 @@
 import React from "react";
 import AboutUs from "../components/features/AboutUs";
+import ContactUs from "../components/marketing/ContactUs";
+// import ContactForm from "../components/marketing/ContactForm";
 
 const AboutPage = () => {
-	return <AboutUs />;
+	return (
+		<>
+			<AboutUs />
+			<ContactUs />
+		</>
+	);
 };
 
 export default AboutPage;
