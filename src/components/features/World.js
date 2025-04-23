f58330e5c1afdc6ca49f757baf0a479844ad4630
Jande Royalty <janderoyalty@gmail.com>
2025-04-23 04:43:08 -0500

    Update world modal and learn more button

--- src/components/features/World.js
+++ src/components/features/World.js
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./World.css";
 import WorldText from "../game-info/WorldText";
+import Button from "react-bootstrap/Button";
+import { Modal } from "react-bootstrap";
 
 const World = () => {
-  return (
-    <div className="content" id="world">
-      <div id="world--map">
-        <img
-          id="world--map-image"
-          src="https://firebasestorage.googleapis.com/v0/b/moratia-games.appspot.com/o/moratia_world_map.png?alt=media&token=c15e87bc-b13b-464b-b350-33673b21fd69"
-          alt="map of the world of Moratia"
-        ></img>
-        <div id="world--map--text">
-          <WorldText></WorldText>
-        </div>
-      </div>
-    </div>
-  );
+	const [showModal, setShowModal] = useState(false);
+	const handleCloseModal = () => setShowModal(false);
+	const handleShowModal = () => setShowModal(true);
+
+	return (
+		<div className="content" id="world">
+			<Button
+				variant="outline-light"
+				className="world--map--button"
+				onClick={handleShowModal}
+			>
+				Learn More
+			</Button>
+			<Modal show={showModal} onHide={handleCloseModal} size="lg">
+				<Modal.Header closeButton>
+					<Modal.Title className="world-modal-title">
+						The World of Moratia
+					</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<WorldText></WorldText>
+				</Modal.Body>
+			</Modal>
+			<div id="world--map">
+				<img
+					id="world--map-image"
+					src="https://firebasestorage.googleapis.com/v0/b/moratia-games.appspot.com/o/world%2FIMG_2590.PNG?alt=media&token=f79015ec-febf-47b9-9ca3-9d97d2d26c08"
+					alt="map of the world of Moratia"
+				></img>
+			</div>
+		</div>
+	);
 };
 
 export default World;
--- src/components/game-info/WorldText.js
+++ src/components/game-info/WorldText.js
@@ -2,36 +2,34 @@ import React from "react";
 import "./WorldText.css";
 
 const WorldText = () => {
-  return (
-    <div id="world-text">
-      <div id="world--headers-text">The World of Moratia</div>
-      {/* <div className="sub-headers">What is Moratia</div> */}
-      <div className="body-text" id="world--text-box">
-        <div>
-          Moratia is a fantasy world created and developed by Thomas Royalty as
-          a host for various games and stories. This envisioned reality is
-          rooted in a special essence known as trabia. Trabia permeates
-          everything, and when mastered can be manipulated to nearly limitless
-          skills and techniques. The planet’s rich history spreads across nearly
-          twenty thousand years and includes civilizations that advance and
-          decline. Through the eras of this world, one can find a range of
-          civilizations and societies ranging from the most simplistic to most
-          advanced in both technology and trabiatic techniques. In it’s first
-          iteration, the Era of Barrier Fall, the world is at a crossroads where
-          trabia and trabiatic techniques are being rediscovered after hundreds
-          of years of its rejection and loss to eras past. This reemergence of
-          trabiatic knowledge is put to use in amplifying, advancing and merging
-          with technology to astounding results. In this world, the times of
-          legendary mages past and mechanical advancements present collide into
-          a volatile mix, thrusting individuals and governments into conflict as
-          competition for control of this advancement lies within reach of every
-          man. In this world, you will find your adventure before you. In this
-          world lies your own chance to become a legend remembered through the
-          ages.
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div id="world-text">
+			<div className="body-text" id="world--text-box">
+				<div>
+					Moratia is a fantasy world created and developed by Thomas Royalty as
+					a host for various games and stories. This envisioned reality is
+					rooted in a special essence known as trabia. Trabia permeates
+					everything, and when mastered can be manipulated to nearly limitless
+					skills and techniques. The planet’s rich history spreads across nearly
+					twenty thousand years and includes civilizations that advance and
+					decline. Through the eras of this world, one can find a range of
+					civilizations and societies ranging from the most simplistic to most
+					advanced in both technology and trabiatic techniques. In it’s first
+					iteration, the Era of Barrier Fall, the world is at a crossroads where
+					trabia and trabiatic techniques are being rediscovered after hundreds
+					of years of its rejection and loss to eras past. This reemergence of
+					trabiatic knowledge is put to use in amplifying, advancing and merging
+					with technology to astounding results. In this world, the times of
+					legendary mages past and mechanical advancements present collide into
+					a volatile mix, thrusting individuals and governments into conflict as
+					competition for control of this advancement lies within reach of every
+					man. In this world, you will find your adventure before you. In this
+					world lies your own chance to become a legend remembered through the
+					ages.
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default WorldText;
