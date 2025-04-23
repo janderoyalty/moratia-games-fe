5a1419b627a5956edfea2583b4bdad64ec5d262f
Jande Royalty <janderoyalty@gmail.com>
2025-04-23 02:21:16 -0500

    move modal logic

--- src/components/common/EmbeddedVideo.js
+++ src/components/common/EmbeddedVideo.js
@@ -2,15 +2,15 @@ import React from "react";
 import "../../App.css";
 import { Container } from "react-bootstrap";
 
-const EmbeddedVideo = () => {
+const EmbeddedVideo = ({ how_to_video_url, name }) => {
 	return (
 		<div className="content" id="video">
 			<div className="headers-text">How to Play</div>
 			<Container>
 				<div className="ratio ratio-16x9">
 					<iframe
-						src="https://www.youtube.com/embed/eT-IBcP8uoM?si=LZWH3PjHmiSZIJuJ"
-						title="How to play Moratia CQG"
+						src={how_to_video_url}
+						title={`How to play video for ${name}`}
 						allowFullScreen
 					></iframe>
 				</div>
