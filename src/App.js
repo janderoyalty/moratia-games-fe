d5c5441b84ee2772b34f9d9ee7b0e6a8e43a58c8
Jande Royalty <janderoyalty@gmail.com>
2025-05-15 03:04:53 -0400

    feat(admin): Add AdminNavBar, AdminDataTable, and URLs components
    - Created AdminNavBar.js for navigation in the admin panel
    - Created AdminDataTable.js for displaying data in a table format
    - Created URLs.js for managing URLs with add and edit functionality
    - Updated file structure to include new components

--- src/App.js
+++ src/App.js
@@ -1,54 +1,79 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import NaviBar from "./components/layout/NaviBar";
+import AdminNavBar from "./admin/components/AdminNavBar";
+
 import LandingPage from "./pages/LandingPage";
 import ProductsPage from "./pages/ProductsPage";
 import WorldPage from "./pages/WorldPage";
 import GalleryPage from "./pages/GalleryPage";
 import AboutPage from "./pages/AboutPage";
+
 import SignIn from "./admin/SignIn";
 import ProtectedRoute from "./admin/ProtectedRoute";
-import UpdateEntriesList from "./admin/UpdateEntriesList";
 import UpdateForm from "./admin/UpdateForm";
+import UpdateEntriesList from "./admin/UpdateEntriesList";
+import URLs from "./admin/URLs";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
 	return (
 		<BrowserRouter>
-			<div className="App">
-				<NaviBar />
-				<Routes>
-					<Route path="/" element={<LandingPage />} />
-					<Route path="/products" element={<ProductsPage />} />
-					<Route path="/world" element={<WorldPage />} />
-					<Route path="/gallery" element={<GalleryPage />} />
-					<Route path="/about" element={<AboutPage />} />
-					{/* Conditionally include admin routes */}
-					{process.env.REACT_APP_ADMIN_ENABLED === "true" && (
-						<>
-							<Route path="/signin" element={<SignIn />} />
-							<Route
-								path="/update"
-								element={
-									<ProtectedRoute>
-										<UpdateForm />
-									</ProtectedRoute>
-								}
-							/>
-							<Route
-								path="/update_list"
-								element={
-									<ProtectedRoute>
-										<UpdateEntriesList />
-									</ProtectedRoute>
-								}
-							/>
-						</>
-					)}
-				</Routes>
-			</div>
+			<ContentRouter />
 		</BrowserRouter>
+	);
+}
+
+function ContentRouter() {
+	const location = useLocation();
+	const isAdminPath = location.pathname.startsWith("/admin");
+
+	return (
+		<div className="App">
+			{isAdminPath ? <AdminNavBar /> : <NaviBar />}
+
+			<Routes>
+				{/* Public Routes */}
+				<Route path="/" element={<LandingPage />} />
+				<Route path="/products" element={<ProductsPage />} />
+				<Route path="/world" element={<WorldPage />} />
+				<Route path="/gallery" element={<GalleryPage />} />
+				<Route path="/about" element={<AboutPage />} />
+
+				{/* Admin Routes */}
+				{process.env.REACT_APP_ADMIN_ENABLED === "true" && (
+					<>
+						<Route path="/signin" element={<SignIn />} />
+						<Route
+							path="/update"
+							element={
+								<ProtectedRoute>
+									<UpdateForm />
+								</ProtectedRoute>
+							}
+						/>
+						<Route
+							path="/update_list"
+							element={
+								<ProtectedRoute>
+									<UpdateEntriesList />
+								</ProtectedRoute>
+							}
+						/>
+						<Route
+							path="/admin/urls"
+							element={
+								<ProtectedRoute>
+									<URLs />
+								</ProtectedRoute>
+							}
+						/>
+					</>
+				)}
+			</Routes>
+		</div>
 	);
 }
 
--- src/admin/components/AdminNavBar.js
+++ src/admin/components/AdminNavBar.js
@@ -1,1 +1,63 @@
-// AdminNavBar component file created. Add your content here.
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom";
+import "../../components/layout/NaviBar.css";
+
+const AdminNavBar = () => {
+	const location = useLocation();
+
+	const handleLinkClick = (e, path) => {
+		if (location.pathname === path) {
+			e.preventDefault();
+			window.scrollTo({ top: 0, behavior: "smooth" });
+		}
+	};
+
+	return (
+		<Navbar
+			id="navi-bar"
+			bg="light"
+			variant="light"
+			sticky="top"
+			expand="lg"
+			collapseOnSelect="true"
+			role="navigation"
+		>
+			<Container fluid>
+				<Navbar.Brand
+					as={Link}
+                    to="/"
+                    onClick={(e) => handleLinkClick(e, "/")}
+                    >
+					<img
+						src="https://firebasestorage.googleapis.com/v0/b/moratia-games.appspot.com/o/logos%2FMoratia_Games_logo_black.png?alt=media&token=359efc0c-c776-4ae7-868a-fde70b4581d1"
+						alt="Moratia Games logo black"
+						height="25px"
+					/>
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="navbarScroll" />
+				<Navbar.Collapse id="navbarScroll">
+					<Nav className="ms-auto my-2 my-lg-0" navbarScroll>
+						<Nav.Link as={Link} to="/admin/testimonials">
+							Testimonials
+						</Nav.Link>
+						<Nav.Link as={Link} to="/admin/urls">
+							URLs
+						</Nav.Link>
+						<Nav.Link as={Link} to="/admin/updates">
+							Updates
+						</Nav.Link>
+						<Nav.Link as={Link} to="/admin/photos">
+							Photos
+						</Nav.Link>
+						<Nav.Link as={Link} to="/admin/races">
+							Races
+						</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+};
+
+export default AdminNavBar;
